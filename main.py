4db7bc04b5aa96b1d3b39eac9696f1da8cb9615d
Adithyan T <adithsreedhar628@gmail.com>
2023-09-05 01:08:53 +0530

    project completed

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 from flask import Flask,render_template,request,flash,redirect,url_for
 from werkzeug.utils import secure_filename 
 import os
+import cv2 
 
-UPLOAD_FOLDER = 'static'
+UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'webp', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'super secret key'
 
-def processImage():
+def processImage(filename,operation):
+    print(f"The file name is {filename} and the operation is {operation}")
+    img = cv2.imread(f"uploads/{filename}")
+    match operation:
+        case "cgray":
+            imgProccesed = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            newfilename = f"static/{filename}"
+            cv2.imwrite(newfilename,imgProccesed)
+            return newfilename
+        case "cpng":
+            newfilename = f"static/{filename.split('.')[0]}.png"
+            cv2.imwrite(newfilename,img)
+            return newfilename
+        case "cwebp":
+            newfilename = f"static/{filename.split('.')[0]}.webp"
+            cv2.imwrite(newfilename,img)
+            return newfilename
+        case "cjpg":
+            newfilename = f"static/{filename.split('.')[0]}.jpg"
+            cv2.imwrite(newfilename,img)
+            return newfilename
+        
+
     pass
 
-def allowed_file(filename):
+def allowed_file(filename): 
     return '.' in filename and \
             filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
@@ -27,6 +51,7 @@ def about():
 def edit():
     if request.method =="POST":
         # check if the post request has the file part
+        operation = request.form.get('operation')
         if 'file' not in request.files:
             flash('No file part')
             return "error"
@@ -39,7 +64,8 @@ def edit():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # processImage()
+            new = processImage(filename,operation)
+            flash(f"Your image has been processed and is available <a href='/{new}' target='_blank'> here</a>")
             return render_template('index.html')
 
 app.run(debug=True , port = 7000)
