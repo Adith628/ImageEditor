9338e29c08e07e762c3121123247d523580e01d5
Adithyan T <adithsreedhar628@gmail.com>
2023-09-05 00:12:36 +0530

    File upload into flask in done

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,flash,redirect,url_for
+from werkzeug.utils import secure_filename 
+import os
+
+UPLOAD_FOLDER = 'static'
+ALLOWED_EXTENSIONS = {'webp', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def processImage():
+    pass
+
+def allowed_file(filename):
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/edit', methods =["GET", "POST"])
+def edit():
+    if request.method =="POST":
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return "error"
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return "error no selected file"
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # processImage()
+            return render_template('index.html')
 
 app.run(debug=True , port = 7000)
